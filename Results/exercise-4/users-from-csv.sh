e17bf36dcdf8833dd826dc23ad467c0ecc3db910
velvestad <47135697+velvestad@users.noreply.github.com>
2026-06-02 10:45:43 +0200

    Update to exercise 3 and 4. Added LF line endings to scripts.

--- Results/exercise-4/users-from-csv.sh
+++ Results/exercise-4/users-from-csv.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 csv_file="$1"
 
+# Count lines in csv file to know how many users to create
 lc=$( sed "s/[^0-9]//g" <(wc --lines "$csv_file") )
 
 echo "Username:Password" > username.password
 
-for ((i=1; i<=lc; i++))
+for ((n=1; n<=lc; n++))
 do
-    user=( $( sed -n "${i}p" "$csv_file" | tr -d '\r' ) )
+    # Parse line n in csv file
+    user=( $( sed -n "${n}p" "$csv_file" | tr -d '\r' ) )
     
+    # Parse username and group from the line
     IFS=',' read -r username group <<< "$user"
+
+    # Generate a random 12 character password
     password=$(echo -e "$(tr -dc 'A-Za-z0-9!@#$%^&*()' < /dev/urandom | head -c 12)\n")
 
+    # Creates the user, group and set the password
     useradd $username
+    groupadd $group 2> /dev/null
     echo "$username:$password" | chpasswd
-    groupadd $group
+
+    # Add the username and password to the file
+    echo "$username:$password" >> username.password
 done
